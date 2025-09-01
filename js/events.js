ec21a1ed2bde854580ae62f5b7adc0f73bb5afbf
Danila Dippel <erroraeto@gmail.com>
2025-09-01 21:23:04 +0300

    Update 2025-09-01T18:22:46.216Z

--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-import*as THREE from"https://cdn.jsdelivr.net/npm/three@0.179.1/+esm";let gl=document.querySelector("#gl"),textures=(gl.height=gl.clientHeight,gl.width=gl.clientWidth,["../img/1.png","../img/2.png","../img/3.png","../img/4.png","../img/5.png","../img/6.png","../img/7.png","../img/8.png"].map(e=>(new THREE.TextureLoader).load(e))),alphaTexture=textureLoader.load("../img/5.png"),camera=new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,.01,10),scene=(camera.position.z=1.4,new THREE.Scene),geometry=new THREE.PlaneGeometry(1.5,1,20,20),material=new THREE.ShaderMaterial({uniforms:{uTexture:{value:textures[0]},intensity:{value:.86},spread:{value:97.554},alphaMap:alphaTexture,transparent:!0,color:{value:new THREE.Color("red")},color1:{value:new THREE.Color("red")},color2:{value:new THREE.Color("white")}},vertexShader:`
+import*as THREE from"https://cdn.jsdelivr.net/npm/three@0.179.1/+esm";let gl=document.querySelector("#gl"),textures=(gl.height=gl.clientHeight,gl.width=gl.clientWidth,["../img/1.png","../img/2.png","../img/3.png","../img/4.png","../img/5.png","../img/6.png","../img/7.png","../img/8.png"].map(e=>(new THREE.TextureLoader).load(e))),alphaMapTexture=(new THREE.TextureLoader).load("../img/AlphaMap.png"),camera=new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,.01,10),scene=(camera.position.z=1.4,new THREE.Scene),geometry=new THREE.PlaneGeometry(1.5,1,20,20),material=new THREE.ShaderMaterial({uniforms:{uTexture:{value:textures[0]},intensity:{value:.86},spread:{value:97.554},alphaMap:{value:alphaMapTexture},opacity:{value:1},color:{value:new THREE.Color("red")},color1:{value:new THREE.Color("red")},color2:{value:new THREE.Color("white")},transparent:!0},vertexShader:`
         varying vec2 vUv;
         void main(){
         vUv = uv;
@@ -37,6 +37,9 @@ import*as THREE from"https://cdn.jsdelivr.net/npm/three@0.179.1/+esm";let gl=doc
         // #define TWO_PI PI*2.
 
         uniform sampler2D uTexture;
+        uniform sampler2D alphaMap;
+        uniform float opacity;
+
         uniform vec3 color1;
         uniform vec3 color2;
     
@@ -56,30 +59,39 @@ import*as THREE from"https://cdn.jsdelivr.net/npm/three@0.179.1/+esm";let gl=doc
 
         void main() {
 
-            vec3 c;
-            vec4 Ca = texture2D(uTexture, vUv);
-            vec4 Cb = vec4(vUv.x, vUv.x, vUv.x, 0.0);
-            // vec4 Cb = texture2D(tSec, vUv);
 
 
-            // c = Ca.rgb * Ca.a + Cb.rgb * Cb.a * (1.0 - Ca.a);  // blending equation
-            // gl_FragColor= vec4(c, 1.0);
-            // gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
-
-            float strength = vUv.x * 0.5;
-            float grad = vec4(vec3(strength), 0.0);
-
-            diffuseColor.a *= texture2D( alphaMap, vAlphaMapUv ).g;
+            vec4 texelColor = texture2D(uTexture, vUv);
+            float alpha = texture2D(alphaMap, vUv).r * opacity; // Use red channel for alpha
+            gl_FragColor = vec4(texelColor.rgb, texelColor.a * alpha);
 
 
-            // // y < 0 = transparent, > 1 = opaque
-            // float alpha = smoothstep(0.0, 1.0, vUv.y);
-            // // y < 1 = color1, > 2 = color2
-            // float colorMix = smoothstep(1.0, 2.0, vUv.y);
-            // gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
+
+
+            // vec3 c;
+            // vec4 Ca = texture2D(uTexture, vUv);
+            // vec4 Cb = vec4(vUv.x, vUv.x, vUv.x, 0.0);
+            // // vec4 Cb = texture2D(tSec, vUv);
+
+
+            // // c = Ca.rgb * Ca.a + Cb.rgb * Cb.a * (1.0 - Ca.a);  // blending equation
+            // // gl_FragColor= vec4(c, 1.0);
+            // // gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
+
+            // float strength = vUv.x * 0.5;
+            // float grad = vec4(vec3(strength), 0.0);
+
+            // diffuseColor.a *= texture2D( alphaMap, vAlphaMapUv ).g;
+
+
+            // // // y < 0 = transparent, > 1 = opaque
+            // // float alpha = smoothstep(0.0, 1.0, vUv.y);
+            // // // y < 1 = color1, > 2 = color2
+            // // float colorMix = smoothstep(1.0, 2.0, vUv.y);
+            // // gl_FragColor = vec4(mix(color1, color2, colorMix), alpha);
         }
 
 
 
         // diffuseColor.a *= texture2D( alphaMap, vAlphaMapUv ).g;
-    `});for(let t=0;t<8;t++){let e=material.clone(),r=(e.uniforms.uTexture.value=textures[t%8],new THREE.Mesh(geometry,e));r.position.x=1.7*t,scene.add(r)}let renderer=new THREE.WebGLRenderer({canvas:gl,antialias:!0,alpha:!0}),start=(renderer.setSize(gl.clientWidth,gl.clientHeight),null),isDown=!1,startX;function step(e){var r;start=start||e,"number"==typeof e&&(r=e-start,scene.position.x=0,renderer.render(scene,camera),r<2e3)&&requestAnimationFrame(step),"wheel"==e.type&&(scene.position.x+=.005*-e.deltaX,renderer.render(scene,camera),console.log("Delta X:",e.deltaX)),"mousemove"==e.type&&(r=e.pageX-startX,scene.position.x+=5e-5*r,renderer.render(scene,camera))}gl.addEventListener("wheel",step),gl.addEventListener("mousedown",e=>{isDown=!0,startX=e.pageX}),gl.addEventListener("mousemove",e=>{isDown&&step(e)}),window.addEventListener("mouseup",e=>{isDown=!1,startX=0}),requestAnimationFrame(step);
+    `});for(let o=0;o<8;o++){let e=material.clone(),r=(e.uniforms.uTexture.value=textures[o%8],new THREE.Mesh(geometry,e));r.position.x=1.7*o,scene.add(r)}let renderer=new THREE.WebGLRenderer({canvas:gl,antialias:!0,alpha:!0}),start=(renderer.setSize(gl.clientWidth,gl.clientHeight),null),isDown=!1,startX;function step(e){var r;start=start||e,"number"==typeof e&&(r=e-start,scene.position.x=0,renderer.render(scene,camera),r<2e3)&&requestAnimationFrame(step),"wheel"==e.type&&(scene.position.x+=.005*-e.deltaX,renderer.render(scene,camera),console.log("Delta X:",e.deltaX)),"mousemove"==e.type&&(r=e.pageX-startX,scene.position.x+=5e-5*r,renderer.render(scene,camera))}gl.addEventListener("wheel",step),gl.addEventListener("mousedown",e=>{isDown=!0,startX=e.pageX}),gl.addEventListener("mousemove",e=>{isDown&&step(e)}),window.addEventListener("mouseup",e=>{isDown=!1,startX=0}),requestAnimationFrame(step);
